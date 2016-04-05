a4cda61f3255e562cb0a21a65084e97db506300d
David Vanegas <davidvanegas7@hotmail.com>
2016-04-04 23:29:34 -0500

    add materialize-sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree
+//= require materialize-sprockets
+//= require materialize/extras/nouislider
